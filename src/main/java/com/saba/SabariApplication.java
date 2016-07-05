c8192ec0312d66764677ad931008e8811ec33597
sabarinathan soundararajan <sabarnath@gmail.com>
2016-07-05 11:51:55 +0530

    first commit

---
+++ src/main/java/com/saba/SabariApplication.java
@@ -0,0 +1,12 @@
+package com.saba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabariApplication.class, args);
+	}
+}
